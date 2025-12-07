5699757dd3a5cb03bf08b6af0b4c5729ef1189ec
Takiiiiiiii <43725718+Takiiiiiiii@users.noreply.github.com>
2025-12-07 16:16:20 +0800

    Update framework

--- ao486.sv
+++ ao486.sv
@@ -58,6 +58,7 @@ module emu
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -184,9 +185,11 @@ assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 assign LED_DISK[1] = 0;
 assign LED_POWER   = 0;
 assign BUTTONS     = {~ps2_reset_n, 1'b0};
+assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
 assign HDMI_BLACKOUT = 0;
-assign VGA_DISABLE = 0;
+assign HDMI_BOB_DEINT = 0;
+
 
 led hdd_led(clk_sys, |mgmt_req[5:0], LED_DISK[0]);
 led fdd_led(clk_sys, |mgmt_req[7:6], LED_USER);
